4ac13fa111cf359f3cfe9af25ed523342ad0baa8
fengchentian <115384322+fengchentian@users.noreply.github.com>
2022-11-21 16:47:38 +0800

    Add files via upload

---
+++ A.java
@@ -0,0 +1,23 @@
+package an;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		String s= "it takes time to know a person.";
+		int a;
+		a=s.length();
+		System.out.println(a);
+		int b;
+		b=s.indexOf('a');
+		System.out.println(b);
+		System.out.println(s.toUpperCase());
+		String s1=s.replace('a','A');
+		System.out.println(s1);
+		
+		
+		
+
+	}
+
+}
